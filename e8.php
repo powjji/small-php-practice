2f79e60961d777069878ce39ee808540ecf13277
powjji <amirischillin@gmail.com>
2023-08-04 15:44:35 -0700

    e8 edited

--- e8.php
+++ e8.php
@@ -1,19 +1,28 @@
 <?php
 
+//
+//for ($i = 0; $i <4; $i++) {
+//    echo "*";
+//}echo "\n";
+//for ($j = 0; $j < 2 ; $j++) {
+//    echo "*    *";
+//    echo "\n";
+//}
+//for ($k = 0; $k <4; $k++) {
+//    echo "*";
+//}echo "\n";
+//for ($j = 0; $j < 2 ; $j++) {
+//    echo "*    *";
+//    echo "\n";
+//}
+//for ($k = 0; $k <4; $k++) {
+//    echo "*";}
 
-for ($i = 0; $i <4; $i++) {
-    echo "*";
-}echo "\n";
-for ($j = 0; $j < 2 ; $j++) {
-    echo "*    *";
-    echo "\n";
-}
-for ($k = 0; $k <4; $k++) {
-    echo "*";
-}echo "\n";
-for ($j = 0; $j < 2 ; $j++) {
-    echo "*    *";
-    echo "\n";
-}
-for ($k = 0; $k <4; $k++) {
-    echo "*";}
+$list = array();
+$str='1234-STR';
+for ($i = 0; $i <strlen($str) ; $i++) {
+    if ($str[$i] == (int)$str[$i]) {
+        $temp = (int)$str[$i];
+        $list[] = $temp;
+    }
+}var_dump($list);
