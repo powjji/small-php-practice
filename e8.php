17d25e299358ff86f8ce5522f84f3316d4819619
powjji <amirischillin@gmail.com>
2023-08-04 14:37:05 -0700

    e8 added

---
+++ e8.php
@@ -0,0 +1,19 @@
+<?php
+
+
+for ($i = 0; $i <4; $i++) {
+    echo "*";
+}echo "\n";
+for ($j = 0; $j < 2 ; $j++) {
+    echo "*    *";
+    echo "\n";
+}
+for ($k = 0; $k <4; $k++) {
+    echo "*";
+}echo "\n";
+for ($j = 0; $j < 2 ; $j++) {
+    echo "*    *";
+    echo "\n";
+}
+for ($k = 0; $k <4; $k++) {
+    echo "*";}
